8b193b1ba4ca22d0cdc160c54ac4f5d510c715cd
AgustinGutierrez0 <gutierrezaverruz0@hotmail.com>
2022-12-15 09:56:54 -0600

    first commit

--- src/version.c
+++ src/version.c
@@ -8,30 +8,39 @@
 
 #include <string.h>
 #include <stdio.h>
-#include "var.h"
+
+#include "include/var.h"
 
 #define VECTOR_NAME "vector"
-#define VECTOR_VERSION "5.9"
+#define VECTOR_VERSION "0.2.1"
 
-version v;
+version V;
+FILE *verfile;
 
 void VerSaveVar(void)
 {
-    strcpy(v.version, "--version");
-    strcpy(v.help, "--help");
+    strcpy(V.version, "--version");
+    strcpy(V.help, "--help");
     //
-    strcpy(v.namever, VECTOR_NAME);
-    strcat(v.namever, " ");
-    strcpy(v.verver, VECTOR_VERSION);
-    strcat(v.verver, "\n");
-    strcpy(v.builver, "This program was built for x86_64-pc-linux-gnu");
-    strcat(v.builver, "\n");
-    strcpy(v.copyver, "Copyright (C) 2022-2022 Aarch-64 & xdanep.");
-    strcat(v.copyver, "\n");
-    strcpy(v.licever, "GPLv3+ License: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>");
-    strcat(v.licever, "\n\n");
-    strcpy(v.typever, "This is free software: anyone is free to redistribute and modify it.");
-    strcat(v.typever, "\n");
-    strcpy(v.garaver, "There is no WARRANTY, up to the limits permitted by applicable laws.");
-    strcat(v.garaver, "\n");
+    strcpy(V.namever, VECTOR_NAME);
+    strcat(V.namever, " ");
+    strcpy(V.verver, VECTOR_VERSION);
+    strcat(V.verver, "\n");
+    strcpy(V.builver, "This program was built for x86_64-pc-linux-gnu");
+    strcat(V.builver, "\n");
+    strcpy(V.copyver, "Copyright (C) 2022-2022 Aarch-64 & xdanep.");
+    strcat(V.copyver, "\n");
+    strcpy(V.licever, "GPLv3+ License: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>");
+    strcat(V.licever, "\n\n");
+    strcpy(V.typever, "This is free software: anyone is free to redistribute and modify it.");
+    strcat(V.typever, "\n");
+    strcpy(V.garaver, "There is no WARRANTY, up to the limits permitted by applicable laws.");
+    strcat(V.garaver, "\n");
+    
+    verfile=fopen("version.txt","w");
+    
+    fputs("PROYECT NAME: ", verfile);
+    fputs(VECTOR_NAME, verfile);
+    fputs("\nPROYECT VERSION: ", verfile);
+    fputs(VECTOR_VERSION, verfile);
 }
